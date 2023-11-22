0012389055e8b2f9b7cfd9cb57741b8fce2f236f
박현민 <dev.hmpark@gmail.com>
2023-11-22 12:48:21 +0900

    initialData 에 임의로 넣어준 Array 값들을 Item 과 List에 뿌려준다.

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,5 +1,50 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
+export default function TodoItem({ todo }) {
+    // TodoList에서 생성된 initialData array를 하나씩 {todo}로 받아와
+    // TodoItem 컴포넌트를 생성한다.
+    
+    // 기본적으로 labelId는 고유한 값을 가져야 하기때문에, 아래와같이 적용해 준다.
+    const labelId = `checkbox-list-label-${todo.id}`;
 
-export default function TodoItem(){
-    return <h1>TodoItem</h1>;
+    return (
+        // ListItem을 이용하여 배치를 잡아주고, 
+        // 아이템 내부에 체크박스<Checkbox>, 삭제버튼<DeleteIcon>, 수정버튼<EditIcon>을 배치한다.
+        // 아이콘들은 IconButtton으로 감싸 효과 및 클릭 이벤트를 적용해 줄 수 있다.
+        // 모든 부품들은 import 문을 작성해 주어야 한다.
+        // Checkbox의 값은 checked 속성에서 지정할수 있다.{todo.completed}
+        // ListItemText의 값은 primary 속성에서 지정할 수 있다.{todo.text}
+        <ListItem
+            secondaryAction={
+                <IconButton edge="end" aria-label="delete" >
+                    <DeleteIcon />
+                </IconButton>
+            }
+            disablePadding >
+
+            <ListItemButton role={undefined} dense>
+                <ListItemIcon>
+                    <Checkbox
+                        edge="start"
+                        checked={todo.completed}
+                        tabIndex={-1}
+                        disableRipple
+                        inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={todo.text} />
+                <IconButton aria-label="edit">
+                    <EditIcon id={labelId} />
+                </IconButton>
+            </ListItemButton>
+
+        </ListItem>
+    );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,6 @@
 import TodoItem from "./TodoItem";
+import { useState } from "react";
+import { List } from "@mui/material";
 
 const initialData = [
     {id: 1, text: "TodoList 만들기", completed: false},
@@ -6,7 +8,20 @@ const initialData = [
     {id: 3, text: "TodoList 삭제하기", completed: true},
 ]
 export default function TodoList(){
+    // useState 를 이용하여 상태를 간편하게 변경할 수 있으며,
+    // 컴포넌트가 렌더링 될때마다 새로운 상태로 업데이트할 수 있음
+    const [todos, setTodos] = useState(initialData);
+
     return (
-        <TodoItem />
+        // Material UI의 List를 이용하여 쉽게 리스트를 가지고옴.
+        // map을 이용하여 todos의 array를 하나씩 불러와 TodoItem으로 보내준다.
+        // 이때 키값으로 todo.id를 이용한다.
+        // Todo Item에서는 todo 값을 {todo}로 받아 사용해야 한다.
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            {todos.map((todo) =>
+            <TodoItem key={todo.id} todo={todo} />
+        )}
+            
+        </List>
     );
 }
